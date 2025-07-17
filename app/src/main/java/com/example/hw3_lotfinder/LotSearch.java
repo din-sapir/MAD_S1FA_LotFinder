0e5c310c643ad7c63900771780690cae9b682337
dinsapir <sapid.isp@gmail.com>
2025-07-17 15:40:34 +0300

    Information UI edited and screen transition checked

--- app/src/main/java/com/example/hw3_lotfinder/LotSearch.java
+++ app/src/main/java/com/example/hw3_lotfinder/LotSearch.java
@@ -3,6 +3,8 @@ package com.example.hw3_lotfinder;
 import android.content.Intent;
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -56,5 +58,8 @@ public class LotSearch extends AppCompatActivity {
                 startActivity(i);
             }
         });
+
+        EditText locationET = findViewById(R.id.locationET);
+        Button button = findViewById(R.id.button_continue);
     }
 }

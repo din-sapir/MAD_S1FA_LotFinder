7c0cc33e8e406a10cfaa97ca2627a5396d45bdf7
dinsapir <sapid.isp@gmail.com>
2025-07-19 12:38:02 +0300

    Status bar in list view color updated

--- app/src/main/java/com/example/hw3_lotfinder/MainActivity.java
+++ app/src/main/java/com/example/hw3_lotfinder/MainActivity.java
@@ -30,9 +30,11 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         // Apply system bar insets to avoid UI overlap
+        View statusBarBackground = findViewById(R.id.statusBarBackground);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            statusBarBackground.getLayoutParams().height = systemBars.top;
+            statusBarBackground.requestLayout();
             return insets;
         });
 
